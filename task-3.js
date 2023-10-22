6a76012233854e00a63ed593500b6ce754685f04
sheremetiewaliena <142352568+sheremetiewaliena@users.noreply.github.com>
2023-10-22 23:11:27 +0300

    homework 1 done

--- task-3.js
+++ task-3.js
@@ -1,3 +1,13 @@
+function getElementWidth(content, padding, border) {
+
+    const conteuner = (Number.parseFloat(content) + (Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2));
+    return(`${conteuner}`);
+}
+
+
+getElementWidth("50px", "8px", "4px");
 console.log(getElementWidth("50px", "8px", "4px")); // 74
+ getElementWidth("60px", "12px", "8.5px");
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+getElementWidth("200px", "0px", "0px");
 console.log(getElementWidth("200px", "0px", "0px")); // 200
